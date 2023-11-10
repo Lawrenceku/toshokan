2d69a1a426d3859351c9184076388aa6c248b191
Lawrence Eniola <lawrencekunmilola@gmail.com>
2023-11-10 15:09:06 +0100

    commit message

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -12,15 +12,15 @@ export default function Login(){
 
 return(
     <section className='flex flex-col items-center overflow-hidden'>
-         <Alert className='w-full absolute top-0' severity="error">There was an error with connection!</Alert> 
+        <Alert className='w-full md:w-80 absolute top-0' severity="error">There was an error with connection!</Alert>
         <div className='w-80  h-80 flex justify-center'>
             <img src={splash} alt="" />
         </div>
-        <TextField required className='mb-4 w-full md:w-auto' id="standard-basic" label="UserName" variant="standard" />
-        <TextField required className='m-4 w-full md:w-auto'  id="standard-basic" type='password' label="password" variant="standard" />
-        <Button  type='submit' className='rounded w-full md:w-1/4  text-white bg-[#001f3f] mt-8 mb-4'>    Login</Button>
+        <TextField required className='mb-4 w-full md:w-80' id="standard-basic" label="UserName" variant="standard" />
+        <TextField required className='m-4 w-full md:w-80'  id="standard-basic" type='password' label="password" variant="standard" />
+        <Button  type='submit' className='rounded-full w-full md:w-80 p-2 font-medium text-white bg-[#001f3f] mt-8 mb-4'>    Login</Button>
         <p className=''>No account?</p>
-        <Button id='up'  className='rounded w-full md:w-1/4 border-2  text-[#001f3f] border-[#001f3f]'>    Sign Up</Button>
+        <Button id='up'  className='rounded-full w-full md:w-80 border-2 p-2 font-medium text-[#001f3f] border-[#001f3f] '>    Sign Up</Button>
          
     </section>
 )
--- src/Pages/onboarding.jsx
+++ src/Pages/onboarding.jsx
@@ -8,7 +8,7 @@ export default function Onboarding(){
     function login(){navigate("./Login")}
    return(
     <section className='flex flex-col items-center'>
-        <div className=' h-auto flex flex-col items-center justify-center w- mt-10'>
+        <div className=' h-auto flex flex-col items-center justify-center w- mt-8'>
         {/* <Slider
   slideClass={'my-slider'}
   infinite={true}
@@ -30,15 +30,18 @@ export default function Onboarding(){
     </div>
 </Slider> */}
         <img className='w-80' src='https://img.freepik.com/free-vector/new-employee-concept-illustration_114360-8899.jpg?w=740&t=st=1699564473~exp=1699565073~hmac=dd1bde6877ff6bfecdc417d4f9e9f742eb915b2963a139e179691b8b18f01de4' alt="" />
+        
+        </div>
+        <h1 className='text-center font-bold text-3xl mt-16'>Welcome</h1>
+        <p className=' p-4  pb-8 text-center'>Welcome to Toshokan, your gateway to a world of literary wonders! We're thrilled to have you on board. </p>
         <div className='flex justify-center'>
             <span className='rounded-full bg-gray-800 h-2 w-2 m-1'></span>
             <span className='rounded-full bg-gray-800 h-2 w-2 m-1'></span>
             <span className='rounded-full bg-gray-800 h-2 w-2 m-1'></span>
         </div>
+        <div className='w-full flex justify-end md:justify-center mt-8'>
+        <Button onClick={login}  className='font-medium text-white bg-[#001f3f] rounded py-2 px-6' variant="contained">Next <span className='text-xl  ml-1'>&#187;</span></Button>
         </div>
-        <h1 className='text-center font-bold text-3xl mt-16'>Welcome</h1>
-        <p className=' p-4  pb-8 text-center'>Welcome to Toshokan, your gateway to a world of literary wonders! We're thrilled to have you on board. </p>
-        <Button onClick={login}  className='font-medium text-white bg-[#001f3f] rounded py-2 px-6' variant="contained">Next</Button>
     </section>
 
    )
