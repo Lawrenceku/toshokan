f17ea31afcfd3f37d0fc39aab348934ea414e9c4
Lawrence Eniola <lawrencekunmilola@gmail.com>
2023-11-10 15:13:43 +0100

    commit

--- src/Pages/onboarding.jsx
+++ src/Pages/onboarding.jsx
@@ -32,7 +32,7 @@ export default function Onboarding(){
         <img className='w-80' src='https://img.freepik.com/free-vector/new-employee-concept-illustration_114360-8899.jpg?w=740&t=st=1699564473~exp=1699565073~hmac=dd1bde6877ff6bfecdc417d4f9e9f742eb915b2963a139e179691b8b18f01de4' alt="" />
         
         </div>
-        <h1 className='text-center font-bold text-3xl mt-16'>Welcome</h1>
+        <h1 className='text-center font-bold text-3xl mt-10'>Welcome</h1>
         <p className=' p-4  pb-8 text-center'>Welcome to Toshokan, your gateway to a world of literary wonders! We're thrilled to have you on board. </p>
         <div className='flex justify-center'>
             <span className='rounded-full bg-gray-800 h-2 w-2 m-1'></span>
