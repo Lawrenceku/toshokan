7784fff1c7a82e4272fd2ee5ad6d426b9c52dd1b
Lawrence Eniola <lawrencekunmilola@gmail.com>
2023-11-11 18:27:35 +0100

    commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,19 +7,19 @@ export default function Home(){
     const navigate = useNavigate()
     return(
         <section className="flex justify-center items-center">
-            <h1 className="font-bold text-3xl">Home</h1>
+            <h1 className="font-bold text-3xl ">Home</h1>
             <footer className='fixed flex justify-around items-center left-0 bottom-0 w-full h-16 border  shadow-md bg-white'>
      <div className="flex flex-col items-center">
-        <AppsIcon></AppsIcon>
-          <a onClick={navigate("./Home")} className="focus:text-red-800">Home</a>
+        <AppsIcon className="text-[#001f3f] font-bold"></AppsIcon>
+          <button onClick={navigate("/Home")} className="text-[#001f3f] font-bold">Home</button>
      </div>
      <div className="flex flex-col items-center">
         <ExploreIcon></ExploreIcon>
-          <a   onClick={navigate("./Explore")}  className="focus:text-red-800">Explore</a>
+          <button   onClick={navigate("/Explore")} >Explore</button>
      </div>
      <div className="flex flex-col items-center">
         <Person2Icon></Person2Icon>
-          <a  onClick={navigate("./Me")} className="focus:text-red-800">Me</a>
+          <button  onClick={navigate("/Me")}>Me</button>
      </div>
     </footer>
         </section>
--- src/Pages/Me.jsx
+++ src/Pages/Me.jsx
@@ -1,19 +1,25 @@
 import { useNavigate } from "react-router-dom";
+import AppsIcon from '@mui/icons-material/Apps';
+import ExploreIcon from '@mui/icons-material/Explore';
+import Person2Icon from '@mui/icons-material/Person2';
 
-export default function Home(){
+export default function Me(){
     const navigate = useNavigate()
     return(
         <section className="flex justify-center items-center">
-            <h1 className="font-bold text-3xl">Me</h1>
+            <h1 className="font-bold text-3xl ">Me</h1>
             <footer className='fixed flex justify-around items-center left-0 bottom-0 w-full h-16 border  shadow-md bg-white'>
-     <div className="flex flex-col">
-          <a onClick={navigate("./Home")} className="focus:text-red-800">Home</a>
+     <div className="flex flex-col items-center">
+        <AppsIcon ></AppsIcon>
+          <button onClick={navigate("/Home")} >Home</button>
      </div>
-     <div className="flex flex-col ">
-          <a   onClick={navigate("./Explore")}  className="focus:text-red-800">Explore</a>
+     <div className="flex flex-col items-center">
+        <ExploreIcon ></ExploreIcon>
+          <button   onClick={navigate("/Explore")}  >Explore</button>
      </div>
-     <div className="flex flex-col">
-          <a  onClick={navigate("./Me")} className="focus:text-red-800">Me</a>
+     <div className="flex flex-col items-center">
+        <Person2Icon className="text-[#001f3f] font-bold"></Person2Icon>
+          <button className="text-[#001f3f] font-bold" onClick={navigate("/Me")} >Me</button>
      </div>
     </footer>
         </section>
