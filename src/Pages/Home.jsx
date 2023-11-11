b8bc889285299190f5461e48d7cbd4dbf632efd5
Lawrence Eniola <lawrencekunmilola@gmail.com>
2023-11-11 17:28:29 +0100

    commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,19 @@
+import { useNavigate } from "react-router-dom";
 
 export default function Home(){
+    const navigate = useNavigate()
     return(
         <section className="flex justify-center items-center">
             <h1 className="font-bold text-3xl">Home</h1>
             <footer className='fixed flex justify-around items-center left-0 bottom-0 w-full h-16 border  shadow-md bg-white'>
      <div className="flex flex-col">
-          <a href="/home" className="focus:text-red-800">Home</a>
+          <a href="/home" onClick={navigate("./Home")} className="focus:text-red-800">Home</a>
      </div>
      <div className="flex flex-col ">
-          <a href="/Explore" className="focus:text-red-800">Explore</a>
+          <a  href="/Explore" onClick={navigate("./Explore")}  className="focus:text-red-800">Explore</a>
      </div>
      <div className="flex flex-col">
-          <a href="/me" className="focus:text-red-800">Me</a>
+          <a href="/me" onClick={navigate("./Me")} className="focus:text-red-800">Me</a>
      </div>
     </footer>
         </section>
--- src/Pages/Me.jsx
+++ src/Pages/Me.jsx
@@ -5,13 +5,13 @@ export default function Home(){
             <h1 className="font-bold text-3xl">Me</h1>
             <footer className='fixed flex justify-around items-center left-0 bottom-0 w-full h-16 border  shadow-md bg-white'>
      <div className="flex flex-col">
-          <a href="/home" className="focus:text-red-800">Home</a>
+          <a href="/home" onClick={navigate("./Home")} className="focus:text-red-800">Home</a>
      </div>
      <div className="flex flex-col ">
-          <a href="/Explore" className="focus:text-red-800">Explore</a>
+          <a  href="/Explore" onClick={navigate("./Explore")}  className="focus:text-red-800">Explore</a>
      </div>
      <div className="flex flex-col">
-          <a href="/me" className="focus:text-red-800">Me</a>
+          <a href="/me" onClick={navigate("./Me")} className="focus:text-red-800">Me</a>
      </div>
     </footer>
         </section>
