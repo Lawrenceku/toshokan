25bfd7d89f2447c5fb5e1b002d137e69b6167232
Lawrence Eniola <lawrencekunmilola@gmail.com>
2023-11-11 17:32:33 +0100

    cm

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home(){
             <h1 className="font-bold text-3xl">Home</h1>
             <footer className='fixed flex justify-around items-center left-0 bottom-0 w-full h-16 border  shadow-md bg-white'>
      <div className="flex flex-col">
-          <a href="/home" onClick={navigate("./Home")} className="focus:text-red-800">Home</a>
+          <a onClick={navigate("./Home")} className="focus:text-red-800">Home</a>
      </div>
      <div className="flex flex-col ">
           <a  href="/Explore" onClick={navigate("./Explore")}  className="focus:text-red-800">Explore</a>
