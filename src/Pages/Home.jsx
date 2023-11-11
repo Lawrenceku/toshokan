b74c2b05511f02219054ce72a732cd9e353f26cb
Lawrence Eniola <lawrencekunmilola@gmail.com>
2023-11-11 17:55:51 +0100

    icons commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,7 @@
 import { useNavigate } from "react-router-dom";
+import AppsIcon from '@mui/icons-material/Apps';
+import ExploreIcon from '@mui/icons-material/Explore';
+import Person2Icon from '@mui/icons-material/Person2';
 
 export default function Home(){
     const navigate = useNavigate()
@@ -6,13 +9,16 @@ export default function Home(){
         <section className="flex justify-center items-center">
             <h1 className="font-bold text-3xl">Home</h1>
             <footer className='fixed flex justify-around items-center left-0 bottom-0 w-full h-16 border  shadow-md bg-white'>
-     <div className="flex flex-col">
+     <div className="flex flex-col items-center">
+        <AppsIcon></AppsIcon>
           <a onClick={navigate("./Home")} className="focus:text-red-800">Home</a>
      </div>
-     <div className="flex flex-col ">
+     <div className="flex flex-col items-center">
+        <ExploreIcon></ExploreIcon>
           <a   onClick={navigate("./Explore")}  className="focus:text-red-800">Explore</a>
      </div>
-     <div className="flex flex-col">
+     <div className="flex flex-col items-center">
+        <Person2Icon></Person2Icon>
           <a  onClick={navigate("./Me")} className="focus:text-red-800">Me</a>
      </div>
     </footer>
