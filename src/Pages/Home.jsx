b88ce5a815f4dc7d9a9cc140f7e15b358bc65a54
Lawrence Eniola <lawrencekunmilola@gmail.com>
2023-11-12 10:13:24 +0100

    commit mesg

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -27,16 +27,29 @@ export default function Home(){
               <div className="mt-16 flex flex-col">
                 <h1 className="w-full text-5xl font-medium text-left text-gray-900">Recommended</h1>
                 <div className="flex flex-grow ">
-                    <div className="flex h-80 w-80">
-                      <img src="https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg?w=740&t=st=1699740387~exp=1699740987~hmac=81ea4f652a1424f7a38ad7acbe41d0dc9993d093914f75db2b58e7cc4abeeb6f" alt="" />
+                    
+                      <div class="p-10">  
+  
+                    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                    <img class="w-full"  src="https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg?w=740&t=st=1699740387~exp=1699740987~hmac=81ea4f652a1424f7a38ad7acbe41d0dc9993d093914f75db2b58e7cc4abeeb6f" alt="" />
+                      <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">Mountain</div>
+                        <p class="text-gray-700 text-base">
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                        </p>
                       </div>
-                      <div className="flex h-80 w-80">
-                      <img src="https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg?w=740&t=st=1699740387~exp=1699740987~hmac=81ea4f652a1424f7a38ad7acbe41d0dc9993d093914f75db2b58e7cc4abeeb6f" alt="" />
+                      <div class="px-6 pt-4 pb-2">
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
                       </div>
+                    </div>
+                  </div>
+                      
                       
                 </div>
               </div>
-              <section className="rounded-3xl  shadow-[0px_-10px_20px_-20px_rgba(0,0,0,0.3)] ">
+              <section className="rounded-3xl p-4 mt-10 shadow-[0px_-10px_40px_-20px_rgba(0,0,0,0.3)] ">
                 <h1 className="font-medium text-lg ">Your Books (5)</h1>
                 <div className="flex flex-col ">
                   <div className=" w-40 h-40 bg-green-800  ">
