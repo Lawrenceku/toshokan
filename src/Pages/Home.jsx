005aa8526d21dd3d983403a3a151a6a909505315
Lawrence Eniola <lawrencekunmilola@gmail.com>
2023-11-12 11:09:26 +0100

    commit message

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,6 +8,7 @@ import InputBase from '@mui/material/InputBase';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
+import Rating from '@mui/material/Rating';
 //import '/src/assets/application.css'
 
 export default function Home(){
@@ -50,18 +51,19 @@ export default function Home(){
             </Paper>
               </header>
               <div className="mt-16 flex flex-col">
-                <h1 className="w-full p-4 text-4xl font-medium text-left text-gray-900">Recommended</h1>
-                <div className="flex flex-grow ">
+                <h1 className="w-full p-4 text-4xl font-medium text-left font-serif text-gray-900">Recommended</h1>
+                <div className="flex flex-grow  ">
                     
-                      <div class="p-10">  
+                      <div class="p-10 ">  
   
-                    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                    <div class="bg-[#fefaff] max-w-sm rounded overflow-hidden shadow-lg">
                     <img class="w-full"  src="https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg?w=740&t=st=1699740387~exp=1699740987~hmac=81ea4f652a1424f7a38ad7acbe41d0dc9993d093914f75db2b58e7cc4abeeb6f" alt="" />
                       <div class="px-6 py-4">
-                        <div class="font-bold text-xl mb-2">Mountain</div>
+                        <div class="font-bold text-xl mb-2">Engineering Mathematics</div>
                         <p class="text-gray-700 text-base">
                           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
                         </p>
+                        <Rating name="read-only" value={3} readOnly />
                       </div>
                       <div class="px-6 pt-4 pb-2">
                         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#University</span>
@@ -69,6 +71,7 @@ export default function Home(){
                         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Engineering</span>
                       </div>
                     </div>
+
                   </div>
                       
                       
@@ -77,35 +80,57 @@ export default function Home(){
               <section className="rounded-3xl p-4 mt-10 shadow-[0px_-10px_40px_-20px_rgba(0,0,0,0.3)] ">
                 <h1 className="font-medium text-lg ">Your Books (5)</h1>
                 <div className="flex flex-col ">
-                  <div className=" w-40 h-40 bg-green-800  ">
-                  <img className="" src="https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg?w=740&t=st=1699740387~exp=1699740987~hmac=81ea4f652a1424f7a38ad7acbe41d0dc9993d093914f75db2b58e7cc4abeeb6f" alt="" />
-                  </div>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora delectus minima suscipit officia porro atque!</p>
-                  <div className=" w-40 h-40 bg-green-800  ">
-                  <img className="" src="https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg?w=740&t=st=1699740387~exp=1699740987~hmac=81ea4f652a1424f7a38ad7acbe41d0dc9993d093914f75db2b58e7cc4abeeb6f" alt="" />
-                  </div>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora delectus minima suscipit officia porro atque!</p>
-                  <div className=" w-40 h-40 bg-green-800  ">
-                  <img className="" src="https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg?w=740&t=st=1699740387~exp=1699740987~hmac=81ea4f652a1424f7a38ad7acbe41d0dc9993d093914f75db2b58e7cc4abeeb6f" alt="" />
-                  </div>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora delectus minima suscipit officia porro atque!</p>
-                  <div className=" w-40 h-40 bg-green-800  ">
-                  <img className="" src="https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg?w=740&t=st=1699740387~exp=1699740987~hmac=81ea4f652a1424f7a38ad7acbe41d0dc9993d093914f75db2b58e7cc4abeeb6f" alt="" />
-                  </div>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora delectus minima suscipit officia porro atque!</p>
-                  <div className=" w-40 h-40 bg-green-800  ">
-                  <img className="" src="https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg?w=740&t=st=1699740387~exp=1699740987~hmac=81ea4f652a1424f7a38ad7acbe41d0dc9993d093914f75db2b58e7cc4abeeb6f" alt="" />
-                  </div>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora delectus minima suscipit officia porro atque!</p>
-                  <div className=" w-40 h-40 bg-green-800  ">
-                  <img className="" src="https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg?w=740&t=st=1699740387~exp=1699740987~hmac=81ea4f652a1424f7a38ad7acbe41d0dc9993d093914f75db2b58e7cc4abeeb6f" alt="" />
-                  </div>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora delectus minima suscipit officia porro atque!</p>
-                  <div className=" w-40 h-40 bg-green-800  ">
-                  <img className="" src="https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg?w=740&t=st=1699740387~exp=1699740987~hmac=81ea4f652a1424f7a38ad7acbe41d0dc9993d093914f75db2b58e7cc4abeeb6f" alt="" />
-                  </div>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora delectus minima suscipit officia porro atque!</p>
+                  
                 
+                <div className="max-w-sm w-full my-8 lg:max-w-full lg:flex hover:shadow-md duration-200">
+      <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{ backgroundImage: "url('https://img.freepik.com/free-vector/good-paper-wattpad-book-cover_23-2149452379.jpg?w=740&t=st=1699783152~exp=1699783752~hmac=4378c1e852586fe6f5541f4eb64f460598512cbd427a466663564812db2450e1')" }} title="Woman holding a mug">
+      </div>
+      <div className=" bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+        <div className="mb-8">
+          
+          <div className="text-gray-900 font-bold text-xl mb-2">  Looks Good On Paper</div>
+          <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet.consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+        </div>
+        
+      </div>
+    </div>
+
+    <div className="max-w-sm w-full my-8 lg:max-w-full lg:flex hover:shadow-md duration-200">
+      <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{ backgroundImage: "url('https://img.freepik.com/premium-vector/doodle-math-formula-notebook-page_1639-27836.jpg?size=626&ext=jpg&ga=GA1.1.1917407316.1652918962&semt=ais')" }} title="Woman holding a mug">
+      </div>
+      <div className=" bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+        <div className="mb-8">
+          
+          <div className="text-gray-900 font-bold text-xl mb-2">  Maths For Dummies</div>
+          <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet.consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+        </div>
+        
+      </div>
+    </div>
+    <div className="max-w-sm w-full my-8 lg:max-w-full lg:flex hover:shadow-md duration-200">
+      <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{ backgroundImage: "url('https://img.freepik.com/free-vector/chalkboard-with-math-elements_1411-88.jpg?size=626&ext=jpg&ga=GA1.1.1917407316.1652918962&semt=ais')" }} title="Woman holding a mug">
+      </div>
+      <div className=" bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+        <div className="mb-8">
+          
+          <div className="text-gray-900 font-bold text-xl mb-2"> Back To School Calculation</div>
+          <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet.consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+        </div>
+        
+      </div>
+    </div>
+    <div className="max-w-sm w-full my-8 lg:max-w-full lg:flex hover:shadow-md duration-200">
+      <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{ backgroundImage: "url('https://img.freepik.com/free-vector/trigonometry-flyer-template_1284-10156.jpg?size=626&ext=jpg&ga=GA1.1.1917407316.1652918962&semt=ais')" }} title="Woman holding a mug">
+      </div>
+      <div className=" bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+        <div className="mb-8">
+          
+          <div className="text-gray-900 font-bold text-xl mb-2"> Trigonometry For Monkeys</div>
+          <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet.consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+        </div>
+        
+      </div>
+    </div>
                 </div>
               </section>
             </section>
